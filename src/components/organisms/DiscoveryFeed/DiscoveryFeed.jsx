2a9f7a2f3990db768a44cdd2e43ce131767da4df
Basliel Mesfin <baslielamare@gmail.com>
2026-04-11 22:23:24 +0300

    Fix masonry single column bug: removed AnimatePresence wrapper from Masonry grid

--- src/components/organisms/DiscoveryFeed/DiscoveryFeed.jsx
+++ src/components/organisms/DiscoveryFeed/DiscoveryFeed.jsx
@@ -1,25 +1,34 @@
-import { AnimatePresence } from 'framer-motion'
+import Masonry from 'react-masonry-css'
 import ItemCard from '../../molecules/ItemCard/ItemCard'
 import './DiscoveryFeed.css'
 
 export default function DiscoveryFeed({ items, likedItems = new Set(), onItemClick, onLike, onSave }) {
+  const breakpointColumnsObj = {
+    default: 5,
+    1024: 4,
+    768: 3,
+    480: 2
+  };
+
   return (
     <div className="discovery-feed">
-      <div className="masonry-grid">
-        <AnimatePresence mode="popLayout">
-          {items.map((item, index) => (
-            <ItemCard
-              key={item.id}
-              item={item}
-              index={index}
-              isLiked={likedItems.has(item.id)}
-              onClick={onItemClick}
-              onLike={onLike}
-              onSave={onSave}
-            />
-          ))}
-        </AnimatePresence>
-      </div>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {items.map((item, index) => (
+          <ItemCard
+            key={item.id}
+            item={item}
+            index={index}
+            isLiked={likedItems.has(item.id)}
+            onClick={onItemClick}
+            onLike={onLike}
+            onSave={onSave}
+          />
+        ))}
+      </Masonry>
     </div>
   )
 }
